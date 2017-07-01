7e515e456db504d78d06807b90f6ea3d7e3bd371
Céline <celine@celine>
2017-07-01 19:02:38 +0200

    First commit

---
+++ modus/js/effets.js
@@ -0,0 +1,65 @@
+function myMove(elemId, nouvelleTaille)
+{
+	var elem = document.getElementById(elemId);   
+    //alert("elem  :" + elem);
+    //alert("elemelemIdelemId  :" + elemId);
+	var currentHeight = elem.offsetHeight;
+	var diff = nouvelleTaille - currentHeight;
+	//alert("nouvelleTaille : " + nouvelleTaille);
+	//alert("currentHeight : " + currentHeight);
+	//alert("diff : " + diff);
+	var diffAbs = Math.abs(diff);
+	//alert("diff abs: " + Math.abs(diff)); 
+	//if(Math.abs(diff) == -1){ aler("diff négatif"); } else {alert("diff positif");}
+	
+	var id = setInterval(move, 0);
+	
+	function move()
+	{	
+		if (diffAbs == 0 /*&& elem.height == nouvelleTaille*/) {
+			//alert("stop");
+			//elem.style.margin = nouvelleTaille + 'px';
+			clearInterval(id);
+		}
+		else
+		{
+			diffAbs--;
+			//alert("diffAbs : " + diffAbs);
+			if(Math.sign(diff) == -1)
+			{
+				//alert("diff négatif");
+				elem.height = currentHeight - 1;
+			}
+			else
+			{
+				//alert("diff positif");
+				elem.height = currentHeight + 1;
+			}
+			//alert("elem.height : " + elem.height);
+			elem.style.height = elem.height + 'px';
+			//alert("elem.style.height : " + elem.style.height);
+			currentHeight = elem.height;
+			//alert("currentHeight : " + currentHeight);
+		}
+	}
+}
+
+
+//hidden en jquery //# -> id . -> class
+$(document).ready(function()
+{
+	$("#fullDescr").fadeOut();
+	$("#arevaImg").click(function()
+		{
+			$("#arevaDescr").fadeOut();  //fideIn() pour faire apparaitre.
+			// $("#fullDescr").fadeIn(1000);
+			$("#fullDescr").slideToggle("slow", "swing"); //descend puis monte
+			//$("#fullDescr").slideDown("slow", "linear");  //descend juste
+			$("#heredis").fadeOut();
+			$("#impika").fadeOut();
+		}
+	);
+	/*$("#arevaImg").click(function(){
+		$("#arevaDescr").fadeIn();
+	});*/
+});
