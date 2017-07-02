f34d419a4ad71de673e138cf30868ba762f0c156
Céline <celine@celine>
2017-07-02 20:26:40 +0200

    Renaming of 'areva.png', 'heredis.png' and 'impika.png' to 'arevaImg.png', 'heredisImg.png' and 'impikaImg.png'. Animation when you click on the "mini" Areva : the 3 columns disappear and the full description appear. Animation when you click anywhere on the screen (when the full description is shown): the full description and the entreprise image associated desappear. And the 3 columns appear.

--- modus/js/effets.js
+++ modus/js/effets.js
@@ -1,67 +1,84 @@
-function myMove(elemId, nouvelleTaille)
-{
-	var elem = document.getElementById(elemId);   
-    //alert("elem  :" + elem);
-    //alert("elemelemIdelemId  :" + elemId);
-	var currentHeight = elem.offsetHeight;
-	var diff = nouvelleTaille - currentHeight;
-	//alert("nouvelleTaille : " + nouvelleTaille);
-	//alert("currentHeight : " + currentHeight);
-	//alert("diff : " + diff);
-	var diffAbs = Math.abs(diff);
-	//alert("diff abs: " + Math.abs(diff)); 
-	//if(Math.abs(diff) == -1){ aler("diff négatif"); } else {alert("diff positif");}
+// function myMove(elemId, nouvelleTaille)
+// {
+// 	var elem = document.getElementById(elemId);   
+//     //alert("elem  :" + elem);
+//     //alert("elemelemIdelemId  :" + elemId);
+// 	var currentHeight = elem.offsetHeight;
+// 	var diff = nouvelleTaille - currentHeight;
+// 	//alert("nouvelleTaille : " + nouvelleTaille);
+// 	//alert("currentHeight : " + currentHeight);
+// 	//alert("diff : " + diff);
+// 	var diffAbs = Math.abs(diff);
+// 	//alert("diff abs: " + Math.abs(diff)); 
+// 	//if(Math.abs(diff) == -1){ aler("diff négatif"); } else {alert("diff positif");}
 	
-	var id = setInterval(move, 0);
+// 	var id = setInterval(move, 0);
 	
-	function move()
-	{	
-		if (diffAbs == 0 /*&& elem.height == nouvelleTaille*/) {
-			//alert("stop");
-			//elem.style.margin = nouvelleTaille + 'px';
-			clearInterval(id);
-		}
-		else
-		{
-			diffAbs--;
-			//alert("diffAbs : " + diffAbs);
-			if(Math.sign(diff) == -1)
-			{
-				//alert("diff négatif");
-				elem.height = currentHeight - 1;
-			}
-			else
-			{
-				//alert("diff positif");
-				elem.height = currentHeight + 1;
-			}
-			//alert("elem.height : " + elem.height);
-			elem.style.height = elem.height + 'px';
-			//alert("elem.style.height : " + elem.style.height);
-			currentHeight = elem.height;
-			//alert("currentHeight : " + currentHeight);
-		}
-	}
-}
+// 	function move()
+// 	{	
+// 		if (diffAbs == 0 /*&& elem.height == nouvelleTaille*/) {
+// 			//alert("stop");
+// 			//elem.style.margin = nouvelleTaille + 'px';
+// 			clearInterval(id);
+// 		}
+// 		else
+// 		{
+// 			diffAbs--;
+// 			//alert("diffAbs : " + diffAbs);
+// 			if(Math.sign(diff) == -1)
+// 			{
+// 				//alert("diff négatif");
+// 				elem.height = currentHeight - 1;
+// 			}
+// 			else
+// 			{
+// 				//alert("diff positif");
+// 				elem.height = currentHeight + 1;
+// 			}
+// 			//alert("elem.height : " + elem.height);
+// 			elem.style.height = elem.height + 'px';
+// 			//alert("elem.style.height : " + elem.style.height);
+// 			currentHeight = elem.height;
+// 			//alert("currentHeight : " + currentHeight);
+// 		}
+// 	}
+// }
 
-
-//$("div").animate({height:'150'}); // Permet de redimensionner un élément (ex: div) avec une animation slideDown/Up
 
 //hidden en jquery //# -> id . -> class
 $(document).ready(function()
 {
+	$("#entreprise").hide();
 	$("#fullDescr").hide();
-	$("#arevaImg").click(function()
-		{
-			$("#arevaDescr").fadeOut();  //fideIn() pour faire apparaitre.
-			// $("#fullDescr").fadeIn(1000);
-			// $("#fullDescr").slideToggle("slow", "swing"); //descend puis monte
-			$("#fullDescr").slideDown("slow", "linear");  //descend juste
-			$("#heredis").fadeOut();
-			$("#impika").fadeOut();
-		}
-	);
-	/*$("#arevaImg").click(function(){
-		$("#arevaDescr").fadeIn();
-	});*/
+	$("#rowDescr").hide();
 });
+
+function move(elemId, evenement)
+{
+	hideImagesAndDescr();
+
+	var imgSrc = 'img/job/'+ elemId +'.png';
+ 	$("#imageEntreprise").attr("src", imgSrc);
+
+	$("#entreprise").fadeIn("slow", "linear");	
+	$("#fullDescr").fadeIn("slow", "linear");  //descend juste
+	$("#rowDescr").slideDown("slow", "linear");
+	evenement.stopPropagation();
+}
+
+function hideImagesAndDescr()
+{
+	//Toutes les classe "image" et "description" vont disparaitre.
+	$(".image").fadeOut(300);
+	$(".description").fadeOut(300);
+}
+
+function resetAffichage()
+{
+	$("#entreprise").fadeOut();
+	$("#fullDescr").fadeOut(350, "linear");  
+	$("#rowDescr").slideUp("slow", "linear"); //descend juste
+
+	$(".image").show();
+	$(".description").show();
+}
