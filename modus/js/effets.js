e29c58420a26927c5a0c962e5ae3ff4d13f41bf9
Celine <Celine@Celine>
2017-07-05 14:00:23 +0200

    changeFullDescr() function terminated. Adding some files (css + js) pour timeline and the div in index.html but nothing works.

--- modus/js/effets.js
+++ modus/js/effets.js
@@ -11,6 +11,8 @@ $(document).ready(function()
 	$("#fullDescr").hide();  //Le div qui contient la description complète de l'exp en entreprise est caché.
 	$("#rowDescr").hide();   //La row complète qui contient les deux div du dessus est cachée.
 	$(".invisible").hide();  //Les éléments qui contiennent la class 'invisible' sont masqués.
+
+	timeline();
 });
 
 /* -------------------------------- JAVASCRIPT -------------------------------*/
@@ -78,13 +80,134 @@ function changeFullDescr(elemId)
 	if(elemId == "heredisImg"){
 		$("h4.h4title").text("Stage de fin d'études"); //Changement du contenu de la balise <h4>
 		$("#fullDescr .contentDescription").text(
-			//document.getElementById("arevaDescr").innerText
-			$("#arevaDescr").text()
+			$("#heredisDescr").text()
 		);
-		alert($("#arevaDescr").text());
-		// alert(document.getElementById("arevaDescr").innerText);
+		//alert($("#arevaDescr").text());
 	}
 	else if(elemId == "impikaImg"){
 		$("h4.h4title").text("1ère expérience professionnelle"); //Changement du contenu de la balise <h4>
+		$("#fullDescr .contentDescription").text(
+			$("#impikaDescr").text()
+		);
 	}
+	else if(elemId == "arevaImg"){
+		$("h4.h4title").text("Stage de 2ème année d'études"); //Changement du contenu de la balise <h4>
+		$("#fullDescr .contentDescription").text(
+			$("#arevaDescr").text()
+		);
+	}
+}
+
+function timeline()
+{
+	var contentText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+
+	// dataRoot : '/'
+	var myMappedObject = [
+			{
+				"isSelected": "true",
+				"taskTitle": "My First Point 1",
+				"taskSubTitle": "January 16th, 2014",
+				"assignDate": "16/01/2014",
+				"taskShortDate": "16 Jan",
+				"taskDetails": "hi <span style=\"color:red\">my html content</span> other text"
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 2",
+				"taskSubTitle": "February 28th, 2014",
+				"assignDate": "28/02/2014",
+				"taskShortDate": "28 Feb",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 3",
+				"taskSubTitle": "March 20th, 2014",
+				"assignDate": "20/04/2014",
+				"taskShortDate": "20 Apr",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 4",
+				"taskSubTitle": "May 20th, 2014",
+				"assignDate": "20/05/2014",
+				"taskShortDate": "20 May",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 5",
+				"taskSubTitle": "July 9th, 2014",
+				"assignDate": "09/07/2014",
+				"taskShortDate": "09 July",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 6",
+				"taskSubTitle": "August 30th, 2014",
+				"assignDate": "30/08/2014",
+				"taskShortDate": "30 Aug",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 7",
+				"taskSubTitle": "September 15th, 2014",
+				"assignDate": "15/09/2014",
+				"taskShortDate": "15 Sep",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 8",
+				"taskSubTitle": "November 1st, 2014",
+				"assignDate": "01/11/2014",
+				"taskShortDate": "01 Nov",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 9",
+				"taskSubTitle": "December 10th, 2014",
+				"assignDate": "10/12/2014",
+				"taskShortDate": "10 Dec",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 10",
+				"taskSubTitle": "January 19th, 2015",
+				"assignDate": "19/01/2015",
+				"taskShortDate": "29 Jan",
+				"taskDetails": contentText
+			},
+			{
+				"isSelected": "",
+				"taskTitle": "My Point 11",
+				"taskSubTitle": "March 3rd, 2015",
+				"assignDate": "03/03/2015",
+				"taskShortDate": "3 Mar",
+				"taskDetails": contentText
+			}
+	];
+
+	var jtLine = $('#myjtline').jTLine({
+		callType: 'jsonObject',
+		structureObj: myMappedObject,
+		map: {
+			"dataRoot": "/",
+			"title": "taskTitle",
+			"subTitle": "taskSubTitle",
+			"dateValue": "assignDate",
+			"pointCnt": "taskShortDate",
+			"bodyCnt": "taskDetails"
+		},
+		distanceMode: 'fixDistance', // 'fixDistance' 'auto' 'predefinedDistance'
+		eventsMinDistance: 60,       // Consider It as Distance Unit "by Pixel"
+		fixDistanceValue: 2,         // if eventsMinDistance = 60 & fixDistanceValue= 2 then the value is: (60*2) = 120 px
+		firstPointMargin: 1          // First Point Margin from the left
+	});
 }
